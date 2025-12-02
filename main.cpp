f51f5a0fca62aee770343a4b01197ca0285b4408
yeeYash <thakareyash99258@gmail.com>
2025-12-02 19:34:17 +0530

    DataSet loaded, column figured out.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include "include/rapidcsv.h"
+#include <cmath>
 #include <iostream>
-#include <Eigen/Dense>
+// #include <Eigen/Dense>
 
 int main(){
     
-    Eigen::MatrixXd m(2,2);
-    m << 1, 2, 3, 4;
-    std::cout << "Eigen works" << m << std::endl;
-
-    rapidcsv::Document doc("data/dummy.csv", rapidcsv::LabelParams(0,0));
-    std::cout << "rapidcsv header found" << std::endl;
+    rapidcsv::Document df("data/train.csv");
+    auto col = df.GetColumn<float>("label");
+    
+    std::cout << col.size();
 
     return 0;
 
